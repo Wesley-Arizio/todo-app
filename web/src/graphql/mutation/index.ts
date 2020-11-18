871eb82fc068447ce1e1cf6a044b62b510f273ff
wesley arizio <45431385+Wesley-Arizio@users.noreply.github.com>
2020-11-18 00:21:51 -0300

    fixing identation and removing properties to the updateTask return

--- web/src/graphql/mutation/index.ts
+++ web/src/graphql/mutation/index.ts
@@ -2,43 +2,41 @@ import { gql } from '@apollo/client';
 
 export const CREATE_TASK = gql`
     mutation createTask(
-    $title: String!
-    $description: String!
-    $status: String!
+        $title: String!
+        $description: String!
+        $status: String!
     ){
-    createTask(data: {
-        title: $title,
-        description: $description,
-        status: $status
-    }) { 
-        title
-        description
-        status
-    }
+        createTask( data: {
+            title: $title,
+            description: $description,
+            status: $status
+        }){
+            _id
+        }
     }
 `;
 
 export const UPDATE_TASK = gql`
     mutation updateTask(
-    $id: ID!
-    $title: String!
-    $description: String!
-    $status: String!
+        $id: ID!
+        $title: String!
+        $description: String!
+        $status: String!
     ){
     updateTask(
         id: $id,
         data: {
-        title: $title,
-        description: $description,
-        status: $status
-        }) {
-        _id
+            title: $title,
+            description: $description,
+            status: $status
+        }){
+            _id
         }
     }
 `;
 
 export const REMOVE_ITEM = gql`
     mutation deleteTask($id: ID!) {
-    deleteTask(id: $id)
+        deleteTask(id: $id)
     }
 `;
