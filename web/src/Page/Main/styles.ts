77a29fa76b5c83f2f08add159a2fced1a07733e9
wesley arizio <45431385+Wesley-Arizio@users.noreply.github.com>
2020-11-18 00:29:09 -0300

    fixing page height issue and adding padding

--- web/src/Page/Main/styles.ts
+++ web/src/Page/Main/styles.ts
@@ -2,7 +2,9 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     width: 100vw;
-    height: 100vh;
+    height: auto;
+    min-height: 100vh;
+    padding: 50px 0;
 
     display: flex;
     justify-content: center;
